c67fe54dad91149903c81a6df8c2069b97f10735
Rubens Junior <rubensshureikan@gmail.com>
2022-01-13 18:01:57 -0300

    Find movies

---
+++ backend/src/main/java/com/dev/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,14 @@
+package com.dev.dsmovie.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	Page<Movie> findAll(Pageable pageable);
+
+	
+}
